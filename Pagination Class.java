f71e229b477c9c9d06c6d50164dddda314da6e39
Aditya M <aditya.mohandasan@gmail.com>
2015-05-26 17:51:42 +0530

    Revert "Pagination"
    This reverts commit da96cb8765ce5a2c064848bced16eaab5ec67066.

--- Pagination Class.java
+++ Pagination Class.java
@@ -14,7 +14,7 @@ public class listOfContacts {
                                                                                         Phone,
                                                                                         Birthdate
                                                                                         FROM Contact]));
-        setCon.setPageSize(5);
+       // setCon.setPageSize(5);
         }
 
             
@@ -27,51 +27,5 @@ public class listOfContacts {
     public List <Contact> getContacts() {
         return (List<Contact>) setCon.getRecords();
     }
-
-    public Boolean hasNext{
-       get {
-        return setCon.getHasNext();
-       }
-
-       set; 
-    }
-
-    public Boolean hasPrevious{
-        get{
-            return setCon.getHasPrevious();
-        }
-
-        set;
-    }
-
-    public void first() {
-        setCon.first();
-    }
-
-    public void last() {
-        setCon.last();
-    }
-
-    public void next() {
-        setCon.next();
-    }
-
-    public void previous() {
-        setCon.previous();
-    }
-
-    public Integer pageNumber {
-        get{
-            return setCon.getPageNumber();
-        }
-        set;
-    }
     
-    public Integer getTotalPages() {
-        Decimal totalSize = setCon.getResultSize();
-        Decimal pageSize = setCon.getPageSize();
-         Decimal pages = totalSize/pageSize;
-
-         return (Integer)pages.round(System.RoundingMode.CEILING);
-    }
 }
